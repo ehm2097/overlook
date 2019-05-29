1847bd56a1907c110986e739f8b141517413f6c3
Andrei Vatasescu <37506053+ehm2097@users.noreply.github.com>
2019-05-29 20:02:20 +0300

    data-objects-1
    Started work on data objects plus minor dependency cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -12,8 +12,6 @@ module.exports = function(grunt) {
                     { src: "node_modules/bootstrap/dist/css/bootstrap.css", dest: "dest/css/lib/bootstrap.css" },
                     { src: "node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css", dest: "dest/css/lib/ui-bootstrap.css" },
                     { src: "node_modules/bootstrap/dist/js/bootstrap.js", dest: "dest/js/lib/bootstrap.js" },
-                    { src: "node_modules/gijgo/js/gijgo.js", dest: "dest/js/lib/gijgo.js" },
-                    { src: "node_modules/gijgo/css/gijgo.css", dest: "dest/css/lib/gijgo.css" },
                     { src: "src/index.html", dest: "dest/index.html" },
                     { src: "src/overlook/css/overlook.css", dest: "dest/css/overlook.css" },
                     { expand: true, cwd: "src/overlook/template", src: "**", dest: "dest/template/overlook" },
---
+++ src/overlook/js/data/data-objects-spec.js
@@ -0,0 +1,60 @@
+
+describe("Data object manager", function(){
+
+    var dataObjects;
+    var factory;
+
+    var sample = {
+        prop1: "ABC",
+        prop2: 123
+    }
+    function Handler1(value){
+        expect(value).toEqual(sample.prop1);
+        this.value = sample.prop1;
+    }
+
+    function Handler2(value){
+        expect(value).toEqual(sample.prop2);
+        this.value = sample.prop2;
+    }
+
+
+    beforeEach(angular.mock.module("overlook"));
+
+    beforeEach(inject(function(_okDataObjects_){
+        dataObjects = _okDataObjects_;
+    }));
+
+    it("should be available", function(){
+        expect(dataObjects).toBeTruthy();
+    })
+
+    beforeEach(function(){
+        factory = dataObjects.createFactory({
+            prop1: Handler1,
+            prop2: Handler2
+        })
+    })
+
+    it("should build dedicated factories", function(){
+        expect(factory).toBeTruthy();
+    })
+
+    describe("Data object", function(){
+
+        it("should wrap and unwrap object data", function(){
+            let wrapped = factory.create(sample);
+            expect(wrapped).toBeTruthy();
+            expect(wrapped.prop1).toBeTruthy();
+            expect(wrapped.prop1.value).toEqual(sample.prop1);
+            expect(wrapped.prop2).toBeTruthy();
+            expect(wrapped.prop2.value).toEqual(sample.prop2);
+            let unwrapped = wrapped.copy({});
+            expect(unwrapped).toBeTruthy();
+            expect(unwrapped.prop1).toEqual(sample.prop1);
+            expect(unwrapped.prop2).toEqual(sample.prop2);
+        })
+
+    })
+
+})
